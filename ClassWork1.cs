8b1409201454a0971f11f208a676eb0909e8754e
LeoneJM-CS <188081003+LeoneJM-CS@users.noreply.github.com>
2025-09-04 08:59:34 -0500

    Add files via upload

---
+++ ClassWork1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ClassWork1
+{
+	internal class ClassWork1()
+	{
+		static void Main(string[] args)
+		{
+			/*bool x = true;
+			while (x == true)
+			{
+				Console.WriteLine("Enter a Grade: ");
+				String grade = Console.ReadLine();
+				if (grade == "A")
+				{
+					Console.WriteLine("Excellent\n");
+                }
+				else if (grade == "B")
+				{
+					Console.WriteLine("Very Good\n");                 
+                }
+				else if (grade == "C")
+				{
+					Console.WriteLine("Good\n");                   
+                }
+				else if (grade == "D")
+				{
+					Console.WriteLine("Work Hard\n");						
+				}
+				else if (grade == "E")
+				{
+					Console.WriteLine("Poor\n");                  
+                }
+				else if (grade == "F")
+				{
+					Console.WriteLine("Very Poor\n");                  
+                }
+				else
+				{
+					x = false;
+				}
+            }*/
+
+			Console.WriteLine("Enter a Number: ");
+			int days = Convert.ToInt32(Console.ReadLine());
+			int years = 0;
+			int weeks = 0;
+			while (days >= 7)
+			{
+				if (days >= 365)
+				{
+					days -= 365;
+					years++;
+				}
+				else if (days >= 7)
+				{
+					days -= 7;
+					weeks++;
+				}
+			}
+            Console.WriteLine("Years: " + years);
+            Console.WriteLine("Weeks: " + weeks);
+            Console.WriteLine("Days: " + days);
+			Console.ReadLine();
+        }
+	}
+}
---
+++ Class_work.cs
@@ -0,0 +1,63 @@
+﻿/*using System;
+
+namespace Class1
+{
+	internal class Class1
+	{
+		static void Main(string[] args)
+		{
+			int balance = 1000;
+			bool x = true;
+			while (x == true) 
+			{
+				Console.WriteLine("Welcome to the ATM \n");
+                Console.WriteLine("1. Check Balance \n");
+                Console.WriteLine("2. Withdraw \n");
+                Console.WriteLine("3. Deposit \n");
+                Console.WriteLine("4. Quit \n");
+				Console.WriteLine("Enter option: ");
+				int option = Convert.ToInt32(Console.ReadLine());
+
+				if (option == 1)
+				{
+					Console.WriteLine("Current Balance: {0} \n", balance);
+				}
+				else if (option == 2)
+				{
+					Console.WriteLine("Enter ammount you want to withdraw: ");
+					int withdraw = Convert.ToInt32(Console.ReadLine());
+					if ((withdraw > 0) && (withdraw < balance))
+					{
+						balance -= withdraw;
+					}
+					else
+					{
+						Console.WriteLine("Insufficiant funds \n");
+					}
+				}
+				else if (option == 3)
+				{
+					Console.WriteLine("Enter ammount to deposit: ");
+					int deposit = Convert.ToInt32(Console.ReadLine());
+					if (deposit > 0)
+					{
+						balance += deposit;
+					}
+					else
+					{
+                        Console.WriteLine("Cannot Deposit {0} amount \n", deposit);
+                    }
+				}
+				else if (option == 4)
+				{
+					Console.WriteLine("Exiting Program");
+					x = false;
+				}
+				else
+				{
+                    Console.WriteLine("Please Enter Valid Input \n");
+                }
+            }
+		}
+	}
+}*/
